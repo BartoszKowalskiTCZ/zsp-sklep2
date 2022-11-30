f766ab7c05c96823aee37947baa2579bf6a0f11c
BartoszKowalskiTCZ <bartek.kowalski20052004@gmail.com>
2022-11-30 15:37:54 +0100

    x

---
+++ zapytania.sql
@@ -0,0 +1,6 @@
+// uzytkownicy //
+INSERT INTO Users (name, login, password, is_admin) VALUES ('Jan', 'a@gmail.com', 'a', '0')
+INSERT INTO Users (name, login, password, is_admin) VALUES ('Maja', 'b@gmail.com', 'b','0')
+INSERT INTO Users (name, login, password, is_admin) VALUES ('Adam', 'c@gmail.com', 'c', '0')
+INSERT INTO Users (name, login, password, is_admin) VALUES ('Janusz', 'd@gmail.com', 'd', '0')
+INSERT INTO Users (name, login, password, is_admin) VALUES ('Pawel', 'e@gmail.com', 'e', '0')
