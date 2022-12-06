9b1d69c6d25fee81a85ccbc711febc2ba884e340
BartoszKowalskiTCZ <113582324+BartoszKowalskiTCZ@users.noreply.github.com>
2022-12-06 13:53:19 +0100

    Update addoffer.php

--- addoffer.php
+++ addoffer.php
@@ -11,31 +11,29 @@ $con = new mysqli("127.0.0.1","root","","zsp-sklep");
 <div class="sr"> 
 <h2>Stwórz nowa oferte</h2>      
 <?php 
-echo "<b>Zalogowany jako: </b>".$_COOKIE["uzytkownik"].'<br>'; 
+echo "<b>Zalogowany jako: </b>".$_COOKIE["User"].'<br>'; 
 ?>  
 </div> 
 <div class="main"> 
 <form method="post"> 
-                 <p><label>Name: <input type="text" name="name"></label></p> 
-                 <p><label>Description: <input type="text" name="Description"></label></p> 
-                 <p><label>Price: <input type="text" name="Price"></label></p>  
-                 <p> 
-                     <button type="submit"><b>Stwórz</b></button>  
-                     |  
-                     <a href="listaofert.php" class="a2"><b>Cofnij się do ofert.</b></a>                 
-                 </p> 
-             </form> 
-              
-             <?php 
-                 if(isset($_POST["nazwa"]) && isset($_POST["opis"]) && isset($_POST["cena"])) 
-                 { 
-                     $order_add = 'INSERT INTO produkty (nazwa, opis, cena) VALUES ("'.$_POST["nazwa"].'", "'.$_POST["opis"].'", "'.$_POST["cena"].'")';  
-                     $db -> query($order_add); 
-                 } 
-             ?> 
-         </div> 
-         <div class="sr"> 
-             <h4>zsp-sklep</h4> 
-         </div> 
-     </body> 
- </html> 
+<p><label>Name: <input type="text" name="name"></label></p> 
+<p><label>Description: <input type="text" name="Description"></label></p> 
+<p><label>Price: <input type="text" name="Price"></label></p>  
+<p> 
+<button type="submit"><b>Stwórz</b></button>   
+<a href="listaofert.php" class="a2"><b>Cofnij się do ofert.</b></a>                 
+</p> 
+</form>              
+<?php 
+if(isset($_POST["name"]) && isset($_POST["description"]) && isset($_POST["price"])) 
+{
+$order_add = 'INSERT INTO Products (name, description, price) VALUES ("'.$_POST["name"].'", "'.$_POST["description"].'", "'.$_POST["pricr"].'")';  
+$db -> query($order_add); 
+} 
+?> 
+</div> 
+<div class="sr"> 
+<h4>zsp-sklep</h4> 
+</div> 
+</body> 
+</html> 
