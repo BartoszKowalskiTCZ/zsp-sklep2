2fbe00e162958eb9ed16d5257e8558349134e3f4
BartoszKowalskiTCZ <113582324+BartoszKowalskiTCZ@users.noreply.github.com>
2022-12-06 12:53:24 +0100

    Update listaofert.php

--- listaofert.php
+++ listaofert.php
@@ -9,21 +9,21 @@ $con = new mysqli("127.0.0.1" , " root", " ", "zsp-sklep");
 $announcement = ("SELECT name, description, price FROM products");
 $Orders = ("SELECT products_id, users_id FROM Orders");
 &sql =("SELECT products.id, products.name, products.description, Orders.products_id, Orders.users_id FROM `products` LEFT JOIN Orders ON products.id = Orders.products_id WHERE Users_id IS NULL");
-$offers= $res->fetch_all(MYSQLI_ASSOC);
+$offers = $res->fetch_all(MYSQLI_ASSOC);
 ?>
 </head>
 <body>
 <?php 
-echo "twoje oferty<br>"; 
-for($i = 0; $i < count($y2); $i++){ 
-echo 'Numer ogłoszenia to '.$y2[$i]['id'].'<br>'; 
-echo 'Nazwa produktu '.$y2[$i]['product_name'].'<br>'; 
-echo 'Cena produktu '.$y2[$i]['price'].'<br>'; 
-echo 'Opisz<br>'.$y2[$i]['content'].'<br>'; 
+echo "TWOJE OFERTY <br>"; 
+for($i = 0; $i < count($offers); $i++){ 
+echo 'Numer ogłoszenia '.$offers[$i]['id'].'<br>'; 
+echo 'Nazwa produktu '.$offers[$i]['product_name'].'<br>'; 
+echo 'Cena produktu '.$offers[$i]['price'].'<br>'; 
+echo 'Opis produktu <br>'.$offers[$i]['content'].'<br>'; 
 } 
 ?> 
-<form action="add_offer.php"> 
-<button type="submit">Dodaj nową oferte</button> 
+<form action="addoffer.php"> 
+<button type="submit">DODAJ NOWA OFERTE</button> 
 </form> 
 <?php 
 $x->close(); 
