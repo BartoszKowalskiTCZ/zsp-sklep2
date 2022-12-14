2c4b57b51323249337e15d5ef5e90b2b165600be
BartoszKowalskiTCZ <113582324+BartoszKowalskiTCZ@users.noreply.github.com>
2022-12-14 14:26:22 +0100

    Update listaofert.php

--- listaofert.php
+++ listaofert.php
@@ -5,11 +5,11 @@
     <link rel="stylesheet" href="style.css">     
    <?php
 session_start();
-$con = new mysqli("127.0.0.1" , " root", " ", "zsp-sklep");
-$announcement = ("SELECT name, description, price FROM products");
+$con = new mysqli("127.0.0.1" , "root", "", "zsp-sklep");
+$offer = ("SELECT name, description, price FROM products");
 $Orders = ("SELECT products_id, users_id FROM Orders");
-&sql =("SELECT products.id, products.name, products.description, Orders.products_id, Orders.users_id FROM `products` LEFT JOIN Orders ON products.id = Orders.products_id WHERE Users_id IS NULL");
-$offers = $res->fetch_all(MYSQLI_ASSOC);
+&sql =("SELECT products_id, products_name, products_description, Orders_products_id, Orders_users_id FROM `products` LEFT JOIN Orders ON products.id = Orders_products_id WHERE Users_id IS NULL");
+$con = $res->fetch_all(MYSQLI_ASSOC);
 ?>
 </head>
 <body>
@@ -26,7 +26,7 @@ echo 'Opis produktu <br>'.$offers[$i]['content'].'<br>';
 <button type="submit">DODAJ NOWA OFERTE</button> 
 </form> 
 <?php 
-$x->close(); 
+$con->close(); 
 ?> 
 </body> 
 </html>
