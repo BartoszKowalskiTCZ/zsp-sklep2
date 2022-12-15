dfe776a1bd548f2dbbdae2b0788ad4e1cf151d79
BartoszKowalskiTCZ <113582324+BartoszKowalskiTCZ@users.noreply.github.com>
2022-12-15 10:36:53 +0100

    Update zapytania.sql

--- zapytania.sql
+++ zapytania.sql
@@ -1,20 +1,20 @@
-// dodaje uzytkownikow //
+// DODAJE UZYTKOWNIKOW //
 INSERT INTO `Users` (`name`, `login`, `password`, `is_admin`) VALUES ('Jan', 'a@gmail.com', 'a', '0')
 INSERT INTO `Users` (`name`, `login`, `password`, `is_admin`) VALUES ('Maja', 'b@gmail.com', 'b','0')
 INSERT INTO `Users` (`name`, `login`, `password`, `is_admin`) VALUES ('Adam', 'c@gmail.com', 'c', '0')
 INSERT INTO `Users` (`name`, `login`, `password`, `is_admin`) VALUES ('Janusz', 'd@gmail.com', 'd', '0')
 INSERT INTO `Users` (`name`, `login`, `password`, `is_admin`) VALUES ('Pawel', 'e@gmail.com', 'e', '0')
- // dodaje produkty //
-INSERT INTO `Products` (`name`, `description`, `price`) VALUES ('telefon','bezuzyteczny','100'),('tablet','bialy','120'),('komputer','idealny do grania','3000');  
-
+ // DODAJE PRODUKTY //
+INSERT INTO `Products` (`name`, `description`, `price`) VALUES ('telefon','bezuzyteczny','100'),('tablet','bialy','120'),('komputer','idealny do grania','3000'); 
 //dodaje zamowienia// 
+
 INSERT INTO `Orders` (`Products_id`, `Users_id`) VALUES ('1', '3'), ('3', '1'), ('2', '4'), ('2', '2');
 
-//dodaj nowa oferte //
+//DODAJE NOWA OFERTE //
 INSERT INTO `offers`(`user_id`, `offer_name`, `description`, `price`) 
 VALUES ('1','telefon','bialy','250');
 
-//szczegoly  zakupionego zamowienia//
+///SZCZEGOLY ZAKUPIONEGO ZAMOWIENIA////
 SELECT orders.offer_id, orders.user_id, offers.offer_name, offers.description, offers.price 
 FROM orders JOIN offers ON orders.offer_id=offers.id where orders.user_id="4"
 AND orders.offer_id="1";
@@ -36,7 +36,6 @@ offers.price="[value-3]" WHERE offers.id="2";
 
 //wyswietl szczegoly danej oferty//
 SELECT offers.offer_name, offers.description, offers.price from offers where offers.id="1";
-
 
 //Lista moich zakupow//
 SELECT orders.user_id, offers.offer_name, offers.price FROM orders 
